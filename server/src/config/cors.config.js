35869013c92e5bb1e6ddc635f5d0b13eae57b2e5
sumit devs <mysumitdevs@gmail.com>
2025-06-11 19:37:09 +0530

    logo addded

--- server/src/config/cors.config.js
+++ server/src/config/cors.config.js
@@ -1,16 +1,15 @@
-const allowedOrigins = [
-    'http://localhost:5173/'
-];
+// Remove trailing slash from origin URL
+const allowedOrigins = ['http://localhost:5173'];
 
 export const corsOptions = {
-    origin : (origin,cb)=>{
-        if(!origin) return cb(null,true);
-        if(allowedOrigins.includes(origin)){
-            return cb(null,true);
-        }
-        return callback(new Error('not allowed by cors'));
-    },
-    Credential : true,
-    methods : ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-}
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true, // ✅ must be lowercase
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
