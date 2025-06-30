f7be7add21f47a80a42513c6f3e8974e274d8df6
sumit devs <mysumitdevs@gmail.com>
2025-06-30 21:47:38 +0530

    auth conflict solved

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -26,32 +26,12 @@ export const register = async (req, res) => {
   }
 };
 
-<<<<<<< HEAD
 export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await findUserByEmail(email);
     if (!user) {
       return res.status(400).json({ message: "invalid credentials" });
-=======
-export const login = async (req,res)=>{
-    try{
-        const {email,password} = req.body;
-        const user = await findUserByEmail(email);
-        if(!user){
-            return res.status(400).json({message:'invalid credentials'});
-        }
-        const ismatch = await comparePassword(password,user.password);
-        if(!ismatch){
-            return res.status(400).json({message:'invalid credentials'});
-        }
-        const token = await signAccessToken({ userId: user._id, name: user.name, email: user.email,role:user.role});
-        res.cookie('token',token,{httpOnly:true, maxAge: 1 * 60 * 1000 });
-        console.log(user);
-        res.status(200).json({message:'login successful',token:token});
-    }catch(error){
-        res.status(500).json({message:'internal error'});
->>>>>>> feature/backend-integration
     }
     const ismatch = await comparePassword(password, user.password);
     if (!ismatch) {
